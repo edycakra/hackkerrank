cbed9f17b76991cf0895cce943d33a317ce0dff4
edycakra <edycakrasinaga@gmail.com>
2019-10-02 14:25:10 +0700

    update

---
+++ algo-implementation/formingmagicsquare.js
@@ -0,0 +1,19 @@
+function formingMagicSquare(s) {
+    var magicSquares = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ];
+
+
+}
+console.log(formingMagicSquare([[4, 9, 2],[3, 5, 7],[8, 1, 5]]))//false
+
+/*logic
+create a matrix for col and for row, sum each row of both matrix, if 15, then it is a magic square
+*/
