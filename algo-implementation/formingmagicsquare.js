a746abc53a6ece0385c63bcafdba599760b260f5
edycakra <edycakrasinaga@gmail.com>
2019-10-05 11:04:46 +0700

    update

--- algo-implementation/formingmagicsquare.js
+++ algo-implementation/formingmagicsquare.js
@@ -1,18 +1,23 @@
-function formingMagicSquare(s) {
-    var magicSquares = [
-        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
-        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
-        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
-        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
-        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
-        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
-        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
-        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
-    ];
-
-
+function cost(s, ss) {
+    s = s.map(a => a.join('')).join('');
+    let cost = 0;
+    for (let i = 0; i < s.length; i++) {
+        cost += Math.abs(parseInt(s[i]) - parseInt(ss[i]));
+    }
+    return cost;
 }
-console.log(formingMagicSquare([[4, 9, 2],[3, 5, 7],[8, 1, 5]]))//false
+
+function formingMagicSquare(s) {
+    // Complete this function
+    const ss = ['618753294','816357492','834159672','438951276','672159834','276951438','294753618','492357816',];
+    let min = 100;
+    for (let i = 0; i < ss.length; i++) {
+        const co = cost(s, ss[i]);
+        if (co < min) min = co;
+    }
+    return min;
+}
+console.log(formingMagicSquare([[4, 9, 2],[3, 5, 7],[8, 1, 5]]))//1
 
 /*logic
 create a matrix for col and for row, sum each row of both matrix, if 15, then it is a magic square
